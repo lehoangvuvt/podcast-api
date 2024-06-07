fcbf42702982ab857970cdd6658c1e24791d099e
Le Hoang Vu <hoangvule100@gmail.com>
2024-06-07 12:39:32 +0000

    feat: add allowed origins

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	crs := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "https://podcast-client.vercel.app"},
+		AllowedOrigins:   []string{"http://localhost:3000", "https://podcast-client.vercel.app", "https://podcast.healing-journey.asia"},
 		AllowCredentials: true,
 	})
 
